7c94a2645c01459ace89ee2c6541ba4e47e20b1d
Billy Bones <mr.fkbillybones@gmail.com>
2024-02-15 13:12:53 +0300

    добавление
    добавление файлов в репозиторий

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import Menu from "./components/Menu/Menu";
+import { createGlobalStyle } from "styled-components";
+
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        margin: 0;
+        padding: 0;
+        min-height: 100vh;
+        width: 100%;
+        background-color: #fdf2e9;
+        font-family: 'Annie Use Your Telescope', cursive;
+    }
+`;
+
+const App = () => {
+    return (
+        <div>
+            <GlobalStyle />
+            <Menu />
+        </div>
+    );
+};
+
+export default App;
